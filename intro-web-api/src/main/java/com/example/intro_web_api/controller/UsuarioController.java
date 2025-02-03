b4476488cd3f52d0e7f99460248e77d2ae41422f
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-02-03 17:03:17 -0300

    Update UsuarioController.java

--- intro-web-api/src/main/java/com/example/intro_web_api/controller/UsuarioController.java
+++ intro-web-api/src/main/java/com/example/intro_web_api/controller/UsuarioController.java
@@ -3,19 +3,34 @@ package com.example.intro_web_api.controller;
 import com.example.intro_web_api.model.Usuario;
 import com.example.intro_web_api.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UsuarioController {
 
     @Autowired
     private UsuarioRepository repository;
 
-    @GetMapping("/users")
+    @GetMapping()
     public List<Usuario> getUsers() {
         return repository.listAll();
+    }
+
+    @GetMapping("/{username}")
+    public Usuario getOne(@PathVariable("username") String username) {
+        return repository.findByUsername(username);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Integer id) {
+        repository.remove(id);
+    }
+
+    @PostMapping()
+    public void postUser(@RequestBody Usuario usuario) {
+        repository.save(usuario);
     }
 }

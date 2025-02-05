2624f67bf6a8b4d2e8f9b4f24b1ce8a6b092cd44
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-02-05 19:26:10 -0300

    mapeamento objeto relacional

---
+++ dio-RESTful-api/src/main/java/pedroleonez/dio_RESTful_api/domain/model/Feature.java
@@ -0,0 +1,7 @@
+package pedroleonez.dio_RESTful_api.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+}
---
+++ dio-RESTful-api/src/main/java/pedroleonez/dio_RESTful_api/domain/model/News.java
@@ -0,0 +1,7 @@
+package pedroleonez.dio_RESTful_api.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+}

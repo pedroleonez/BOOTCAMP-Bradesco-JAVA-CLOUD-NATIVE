3da3ed718bf947c0a48083a1d1ba815db9c9c44d
pedroleonez <147958375+pedroleonez@users.noreply.github.com>
2025-02-06 23:17:14 -0300

    Create UserRepository.java

---
+++ dio-RESTful-api/src/main/java/pedroleonez/dio_RESTful_api/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package pedroleonez.dio_RESTful_api.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pedroleonez.dio_RESTful_api.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
